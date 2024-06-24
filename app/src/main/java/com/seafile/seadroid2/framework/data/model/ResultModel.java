f309924093db10fb2f238656d5137bbb60b24fdf
Alex Arce <aarce@alea-soluciones.com>
2024-06-24 15:38:04 +0200

    [FIX] Update merge conflicts

--- app/src/main/java/com/seafile/seadroid2/framework/data/model/ResultModel.java
+++
@@ -1,8 +0,0 @@
-package com.seafile.seadroid2.framework.data.model;
-
-public class ResultModel{
-    public boolean success;
-    public String error_msg;
-
-    public boolean is_selected = false;
-}
--- app/src/main/java/com/seafile/seadroid2/ui/settings/SettingsActivityViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.seafile.seadroid2.ui.settings;
-
-import com.seafile.seadroid2.ui.base.viewmodel.BaseViewModel;
-
-public class SettingsActivityViewModel extends BaseViewModel {
-}
